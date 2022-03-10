ccb19ff5a7f45ea0d12be4e85b22c5a151a4f7f4
Mohammad Mahdi <nemati.mahdi255@gmail.com>
2022-03-11 00:25:25 +0330

    finish number and sign detection

--- Phase 1/lexer.py
+++ Phase 1/lexer.py
@@ -1,60 +1,124 @@
-from ply import lex
+from ply.lex import lex
 
 
 class Lexer:
     reserved = {
-        'IF_KW': r'if',
-        'THEN_KW': r'then',
-        'ELSE_KW': r'else',
-        'WHILE_KW': r'while',
-        'DO_KW': r'do',
+        r'program': 'PROGRAM_KW',
+
+        r'if': 'IF_KW',
+        r'then': 'THEN_KW',
+        r'else': 'ELSE_KW',
+
+        r'while': 'WHILE_KW',
+        r'do': 'DO_KW',
+
+        r'integer': 'INTEGER_KW',
+        r'real': 'REAL_KW',
+
+        r'mod': 'MOD_KW',
+        r'procedure': 'PROCEDURE_KW',
+        r'var': 'VAR_KW',
+        r'div': 'DIV_KW',
+        r'function': 'FUNCTION_KW',
+
+        r'and': 'AND_KW',
+        r'or': 'OR_KW',
+        r'not': 'NOT_KW',
+        r'begin': 'BEGIN_KW',
+        r'end': 'END_KW'
     }
 
     tokens = [
-                 "PROGRAM_KW", "VAR_KW", "EMPTY", "INTEGER_KW",
-                 "REAL_KW", "PROCEDURE_KW", "BEGIN_KW", "END_KW",
-                 "IDENTIFIER", "INTEGER_CONSTANT", "REAL_CONSTANT",
-                 "SUM_KW", "SUB_KW", "MUL_KW", "DIV_KW", "MOD_KW",
-                 "LT_KW", "LE_KW", "EQ_KW", "NE_KW", "GT_KW", "GE_KW",
-                 "AND_KW", "OR_KW", "NOT_KW", "LRB_KW", "RRB_KW", "SEMICOLON",
-             ] + list(reserved.keys())
+                 "INTEGER_CONSTANT", "REAL_CONSTANT", "IDENTIFIER",
+                 "SUM", "SUB", "MUL", "DIV", "POINT", "SIGN", "LT", "LE",
+                 "EQ", "NE", "GT", "GE", "LRB", "RRB",
+                 "ASSIGN", "SEMICOLON", "COLON", "COMMA",
+             ] + list(reserved.values())
 
     # COLONS
     t_SEMICOLON = r'\;'
 
     # BRACKETS
-    t_LRB_KW = r'\('
-    t_RRB_KW = r'\)'
+    t_LRB = r'\('
+    t_RRB = r'\)'
 
     # OPERATORS
-    t_SUM_KW = r'\+'
-    t_SUB_KW = r'\-'
-    t_MUL_KW = r'\*'
-    t_DIV_KW = r'\/'
-    t_MOD_KW = r'\%'
-    t_LT_KW = r'\<'
-    t_LE_KW = r'\<='
-    t_EQ_KW = r'\='
-    t_NE_KW = r'\<>'
-    t_GT_KW = r'\>'
-    t_GE_KW = r'\>='
+    t_MUL = r'\*'
+    t_DIV = r'\/'
+    t_LT = r'\<'
+    t_LE = r'\<='
+    t_EQ = r'\='
+    t_NE = r'\<>'
+    t_GT = r'\>'
+    t_GE = r'\>='
 
     # RESERVE KW
+    t_IF_KW = r'if'
+    t_THEN_KW = r'then'
+    t_ELSE_KW = r'else'
+    t_WHILE_KW = r'while'
+    t_DO_KW = r'do'
 
-    t_IF_KW = r'\if'
-    t_THEN_KW = r'\then'
-    t_ELSE_KW = r'\else'
-    t_WHILE_KW = r'\while'
-    t_DO_KW = r'\do'
+    # A string containing ignored characters (spaces and tabs)
+    t_ignore = '\n \t'
 
-    def t_INTEGER_CONSTANT(self):
-        r"""(?(?=([\d]\s*)(-|\+|\s*[\*|\/])\s*[0]*\K([-|\+|\s]*)(\d)+).*|[-|+]\s*[0]*[\d]+)"""
-        return self
+    @staticmethod
+    def t_SIGN(t):
+        r"""[-|+][-|+|\s]*"""
+        if str(t.value).count('-') % 2 == 0:
+            t.type = 'SUM'
+            t.value = '+'
+        else:
+            t.type = 'SUB'
+            t.value = '-'
+        return t
+
+    # INTEGER REGULAR EXPRESSION
+    def t_INTEGER_CONSTANT(self, t):
+        r"""[\d]+"""
+        t.value = str(t.value).lstrip('0')
+        if len(self.token_list) > 0 and self.token_list[-1].type in ['SUM', 'SUB']:
+            if len(self.token_list) < 1 or (len(self.token_list) > 1 and self.token_list[-2].type != 'INTEGER_CONSTANT'):
+                if self.token_list[-1].type == 'SUB':
+                    t.value = "-" + t.value
+                self.token_list.pop()
+        return t
+
+    # Define a rule so we can track line numbers
+    @staticmethod
+    def t_newline(t):
+        r"""\n+"""
+        t.lexer.lineno += len(t.value)
 
     # Error handling rule
-    def t_error(self):
-        print("Illegal character '%s'" % self)
-        self.lexer.skip(1)
+    @staticmethod
+    def t_error(t):
+        print("Illegal character '%s'" % t.value[0])
+        t.lexer.skip(1)
+
+    def __init__(self, **kwargs):
+        self.lexer = self.__build(**kwargs)
+        self.token_list = []
 
     # Build the lexer
-    lexer = lex.lex()
+    def __build(self, **kwargs):
+        return lex(module=self, **kwargs)
+
+    def input(self, text):
+        self.lexer.input(text)
+
+    def get_tokens(self):
+        token = ''
+        while token is not None:
+            valid, token = self.__token()
+            if valid:
+                self.token_list.append(token)
+        return self.token_list[:-1]
+
+    def __token(self):
+        token = self.lexer.token()
+
+        if token is None:
+            return True, token
+
+        return True, token
--- Phase 1/main.py
+++ Phase 1/main.py
@@ -1,2 +1,13 @@
+from lexer import Lexer
 
-print(2 + - - - + 5)
+lexer = Lexer()
+file = open('../Test/test_INTEGER_CONSTANT.txt')
+text_input = file.read()
+file.close()
+lexer.input(text_input)
+
+tokens = lexer.get_tokens()
+
+for token in tokens:
+    print(token)
+
