c30dd8088d243f193d5e372dec35d18beb5ca1ff
Elaheh Akbari <e.akbari1379@gmail.com>
2022-04-13 19:11:38 +0430

    "Add parser file"

---
+++ Phase 1/parser.py
@@ -0,0 +1,100 @@
+from ply import yacc
+from lexer import Lexer
+
+
+class Parser:
+    tokens = Lexer().tokens
+
+    precedence = (
+        ('left', "LRB", "RRB"),
+
+        ('left', "ELSEIF", "ELSE"),
+        ('left', "IF"),
+
+        ('left', "OR"),
+        ('left', "AND"),
+        ('left', "NOT"),
+
+        ('left', "GT", "LT", "NE", "EQ", "LE", "GE"),
+
+        # ('left', "MOD"),
+        # ('left', "SUM", "SUB"),
+        # ('left', "MUL", "DIV"),
+
+        ('left', "INTEGER_KW"),
+        ('left', "REAL_KW"),
+
+
+        ('left', "ID"),
+        ('right', "ASSIGN"),
+
+        ("left", "ERROR")
+    )
+
+    def __init__(self):
+        pass
+
+    def p_program(self, p):
+        """program : PROGRAM_KW IDENTIFIER declarations procedure_list compound_statement DOT"""
+        print("program : PROGRAM_KW IDENTIFIER declarations procedure_list compound_statement DOT")
+
+    def p_declarations(self, p):
+        """declarations : VAR_KW declaration_list SEMICOLON
+                        | empty"""
+        print("declarations : VAR_KW declaration_list SEMICOLON | empty")
+
+    def p_declaration_list(self, p):
+        """declaration_list : identifier_list  COLON type
+               | declaration_list COLON identifier_list  COLON type"""
+
+        print("declaration_list : identifier_list  COLON type|declaration_list COLON identifier_list  COLON type")
+
+    def p_identifier_list(self, p):
+        """identifier_list : IDENTIFIER
+                | identifier_list COMMA IDENTIFIER"""
+
+        print("identifier_list : IDENTIFIER | identifier_list COMMA IDENTIFIER")
+
+    def p_type(self, p):
+        """type : INTEGER_KW
+                | REAL_KW"""
+        print("type : INTEGER_KW | REAL_KW")
+
+    def p_procedure_list(self, p):
+        """procedure_list : procedure_list procedure"""
+        print("procedure_list : procedure_list procedure")
+
+    def p_procedure(self, p):
+        """procedure : PROCEDURE_KW IDENTIFIER parameters COLON declarations compound_statement
+                    |empty"""
+        print("procedure : PROCEDURE_KW IDENTIFIER parameters COLON declarations compound_statement")
+
+    def p_parameters(self, p):
+        """paramdec : LRB declaration_list RRB"""
+        print("paramdec : LRB declaration_list RRB")
+
+    def p_compound_statement(self, p):
+        """compound_statement : BEGIN_KW statement_list END_KW"""
+        print("compound_statement : BEGIN_KW statement_list END_KW")
+
+    def p_statement_list(self, p):
+        """statement_list : statement
+                     | statement_list COLON statement"""
+        print("statement_list : statement  | statement_list COLON statement")
+
+    def p_statement(self, p):
+        """statement_list : statement
+                     | statement_list COLON statement"""
+        print("statement_list : statement  | statement_list COLON statement")
+
+    def p_empty(self, p):
+        """empty :"""
+        print("empty")
+        pass
+
+    def p_error(self, p):
+        print("Error", p.type, p.lexpos)
+
+    def build(self, **kwargs):
+        self.parser = yacc.yacc(module=self, **kwargs)
+        return self.parser
