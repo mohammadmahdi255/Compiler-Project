518f976ff7a468c4491c75bc6b87e12f4d06a27d
Mohammad Mahdi <nemati.mahdi255@gmail.com>
2022-04-13 22:52:16 +0430

    fix the name of class

--- Phase 1/main.py
+++ Phase 1/main.py
@@ -1,4 +1,4 @@
-from lexer import Lexer
+from Parser import *
 
 file_name = "Final test"
 lexer = Lexer()
@@ -7,10 +7,13 @@ text_input = file.read()
 file.close()
 lexer.input(text_input)
 
-tokens = lexer.get_tokens()
-file = open('../Test/{file_name}_output.txt'.format(file_name=file_name), "w")
+parser = Parser()
+parser.build().parse(text_input, lexer, False)
 
-for token in tokens:
-    file.write(token.__str__() + '\n')
-    print(token)
-file.close()
+# tokens = lexer.get_tokens()
+# file = open('../Test/{file_name}_output.txt'.format(file_name=file_name), "w")
+#
+# for token in tokens:
+#     file.write(token.__str__() + '\n')
+#     print(token)
+# file.close()
