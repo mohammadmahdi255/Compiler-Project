3cd81dda3bcbf725d59db14503be1b6bfe7f2060
Mohammad Mahdi <nemati.mahdi255@gmail.com>
2022-04-14 16:15:30 +0430

    finish resolving

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from Phase_2.Parser import *
+import os
 
 file_name = "Final test"
+path = os.path.dirname(__file__)
 lexer = Lexer().build()
-file = open('../Test/{file_name}.txt'.format(file_name=file_name), "r")
+file = open('{path}\\Test\\{file_name}.txt'.format(path=path, file_name=file_name), "r")
 text_input = file.read()
 file.close()
 lexer.input(text_input)
 
 token = ''
-file = open('../Test/{file_name}_output.txt'.format(file_name=file_name), "w")
+file = open('{path}\\Test\\{file_name}_output.txt'.format(path=path, file_name=file_name), "w")
 while token is not None:
     token = lexer.token()
     file.write(token.__str__() + '\n')
@@ -16,5 +18,3 @@ file.close()
 
 parser = Parser()
 parser.build().parse(text_input, lexer, False)
-
-
